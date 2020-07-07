a08984e895788884fe4efdea995f224c5ed21c5c
parkjihwanjay <cc6656@naver.com>
2020-07-07 16:00:48 +0900

    [add] passwordValidator.js 추가
    * 8~20자
    * 영문,숫자의 조합만 허용

---
+++ public/javascripts/utils/passwordValidator.js
@@ -0,0 +1,14 @@
+function passwordValidator(value){
+    const length = value.length;
+    if(length < 8 || length > 20) return false;
+    return checkEngOrNum(value);
+}
+
+function checkEngOrNum(value){
+    for(let i=0; i<value.length; i++){
+        if(value[i].charCodeAt(0)<48 || value[i].charCodeAt(0)>122) return false;
+    }
+    return true;
+}
+
+module.exports = passwordValidator;
