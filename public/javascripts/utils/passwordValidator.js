833c25745e142ffeec2d7e2f0ef1423acee55002
parkjihwanjay <cc6656@naver.com>
2020-07-07 17:18:43 +0900

    [revise] passwordValidator.js 수정
    * passwordValidator가 상황에 맞는 error 메시지를 반환하도록 수정

--- public/javascripts/utils/passwordValidator.js
+++ public/javascripts/utils/passwordValidator.js
@@ -1,14 +1,15 @@
 function passwordValidator(value){
     const length = value.length;
-    if(length < 8 || length > 20) return false;
+    if(length < 8 || length > 20) return '비밀번호는 8~20자 이내로 작성해주세요';
     return checkEngOrNum(value);
 }
 
 function checkEngOrNum(value){
     for(let i=0; i<value.length; i++){
-        if(value[i].charCodeAt(0)<48 || value[i].charCodeAt(0)>122) return false;
+        if(value[i].charCodeAt(0)<48 || value[i].charCodeAt(0)>122) return '비밀번호는 영문과 숫자로만 작성해주세요';
     }
-    return true;
+    return '';
 }
 
-module.exports = passwordValidator;
+// module.exports = passwordValidator;
+export default passwordValidator;
