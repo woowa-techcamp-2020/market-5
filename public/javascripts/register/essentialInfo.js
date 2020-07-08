fb276eefb2e6b6087f44ac9931f7f3536a336193
parkjihwanjay <cc6656@maver.com>
2020-07-08 11:09:34 +0900

    [revise] 휴대폰 인증받는 로직 추가
    * 2분 타이머 추가
    * 실제 인증번호는 아니고 임시 번호
    * 1234를 입력하면 인증, 아니면 틀렸다고 메시지
    * 휴대폰 번호 입력 받을때 못 지우는 버그 해결

--- public/javascripts/register/essentialInfo.js
+++ public/javascripts/register/essentialInfo.js
@@ -3,20 +3,36 @@ import passwordValidator from '../utils/passwordValidator.js';
 import phoneNumReorder from '../utils/phoneNumReorder.js';
 import emailValidator from '../utils/emailValidator.js';
 
+// 아이디
 const inputId = document.getElementsByName('id')[0];
+// 패스워드
 const inputPassword = document.getElementsByName('password')[0];
 const inputPasswordSecond = document.getElementsByName('passwordSecond')[0]
+// 휴대폰 번호
 const inputPhoneNum = document.getElementById('phone')
+// 이메일
 const inputEmail = document.querySelector('.email-second')
 const selectEmail = document.querySelector('.email')
+// 인증
+const certificationButton = document.querySelector('#certification')
+const verficationButton = document.querySelector('#verficationButton');
 
 inputId.addEventListener('keyup', checkId);
+
 inputPassword.addEventListener('keyup', checkPassword);
 inputPasswordSecond.addEventListener('keyup', comparePassword);
+
 inputPhoneNum.addEventListener('keyup', reorderPhoneNum);
+
 inputEmail.addEventListener('keyup', checkEmail);
 inputEmail.addEventListener('change', checkEmail);
 selectEmail.addEventListener('change', addEmail);
+
+certificationButton.addEventListener('click', certify)
+verficationButton.addEventListener('click', verify)
+
+let interval = null;
+const TIMER_SECONDS = 120;
 
 function addEmail() {
     const email = selectEmail.value;
@@ -97,8 +113,68 @@ function comparePassword() {
     }
 }
 
-function reorderPhoneNum() {
-    const phoneNum = inputPhoneNum.value;
-    const reorderedNum = phoneNumReorder(phoneNum);
-    inputPhoneNum.value = reorderedNum;
+function checkPhoneNum(e){
+    const keyCode = e.keyCode;
+    if(keyCode<48 || keyCode>57){
+        if(keyCode != 8) return false;
+    }
+    return true;
+}
+
+function reorderPhoneNum(e) {
+    if(e.keyCode === 8 ) return;
+    if(checkPhoneNum(e)){
+        const phoneNum = inputPhoneNum.value;
+        const reorderedNum = phoneNumReorder(phoneNum);
+        inputPhoneNum.value = reorderedNum;
+    }else{
+        inputPhoneNum.value = '';
+    }
+}
+
+function countDown(time, timeLeftBox){
+    interval = setInterval(() => {
+        if(time==0) {
+            timeLeftBox.textContent = '입력시간을 초과하였습니다';
+            clearInterval(interval);
+        }
+        const timeLeft = getMinuteAndSecond(time);
+        timeLeftBox.textContent = timeLeft;
+        time--;
+    }, 1000)
+}
+
+function getMinuteAndSecond(time){
+    const minute = Math.floor(time / 60);
+    const second = Math.floor(time % 60);
+    return `${minute} : ${second}`
+}
+
+function certify(e){
+    e.preventDefault();
+    const text = e.target.textContent;
+    const timeLeftBox = document.querySelector('.time-left-box');
+    clearInterval(interval);
+    countDown(TIMER_SECONDS, timeLeftBox);
+    if(text === '인증받기'){
+        timeLeftBox.style.display="block";
+        e.target.textContent = '재전송';
+        document.querySelector('.verification-box').style.display = "block";
+        e.target.style.borderColor = "blue";
+        e.target.style.color = "blue";
+        document.querySelector('.verification-message').style.display = "block";
+    }
+}
+
+function verify(e){
+    e.preventDefault();
+    const verificationNum = document.querySelector('#verificationInput').value;
+    if(verificationNum !== '1234') {
+        document.querySelector('.verification-message').textContent="맞지 않습니다.";
+        document.querySelector('.verification-message').style.color = "red";
+        return;
+    }
+    document.querySelector('.verification-box').style.display = "none";
+    document.querySelector('.time-left-box').style.display = "none";
+    document.querySelector('#certification').textContent = "인증되었습니다"
 }
