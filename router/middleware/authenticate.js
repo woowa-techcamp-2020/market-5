bf04171bb1b93d1e808ff68a50608ac580d794c7
nohgijin <ynda11111@naver.com>
2020-07-10 01:19:22 +0900

    [revise] 쿠키가 아예 없는 경우 처리 쿠키가 아예 없는 경우 로그인이 안되는 문제발생, 예외처리 진행

--- router/middleware/authenticate.js
+++ router/middleware/authenticate.js
@@ -1,37 +1,40 @@
-const {findSessionID} =  require('../../api/register/database.js');
+const { findSessionID } = require('../../api/register/database.js');
 
-function authenticate(req, res, next){
+function authenticate(req, res, next) {
     const cookies = req.headers.cookie;
-    const parsedCookie = cookieParser(cookies);
-    console.log(parsedCookie);
+    if (cookies) {
+        const parsedCookie = cookieParser(cookies);
+        console.log(parsedCookie);
 
-    const sessionID = parsedCookie.sessionID;
-    if(!sessionID) return res.render('login');
-    const id = parsedCookie.id;
+        const sessionID = parsedCookie.sessionID;
+        if (!sessionID) return res.render('login');
+        const id = parsedCookie.id;
 
-    findSessionID(sessionID)
-    .then(session => {
-        console.log('session:', session);
-        if(!session) return res.render('login');
-        req.session = session;
-        return next();
-    })
-    .catch(err => {
-        return res.render('login');
-    })
-
+        findSessionID(sessionID)
+            .then(session => {
+                console.log('session:', session);
+                if (!session) return res.render('login');
+                req.session = session;
+                return next();
+            })
+            .catch(err => {
+                return res.render('login');
+            })
+    } else return res.render('login');
 }
 
-function cookieParser(cookies){
-    const cookieList = cookies.split(';');
-    const parsedCookie = {}
+function cookieParser(cookies) {
+    if (cookies) {
+        const cookieList = cookies.split(';');
+        const parsedCookie = {}
 
-    cookieList.forEach(cookie => {
-        const keyValue = cookie.split('=');
-        parsedCookie[keyValue[0].trim()] = keyValue[1].trim();
-    })
+        cookieList.forEach(cookie => {
+            const keyValue = cookie.split('=');
+            parsedCookie[keyValue[0].trim()] = keyValue[1].trim();
+        })
 
-    return parsedCookie;
+        return parsedCookie;
+    }
 }
 
-module.exports = {authenticate, cookieParser}
+module.exports = { authenticate, cookieParser }
